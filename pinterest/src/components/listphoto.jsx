22ba1afc29d50db0166a8429e577363103f9eaae
jazhina <jazmin12977@gmail.com>
2020-05-12 09:55:20 -0500

    agregando modal

--- pinterest/src/components/listphoto.jsx
+++ pinterest/src/components/listphoto.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-const ListPhoto = (props) => {
-    const photo = props.img;
+const ListPhoto = ({ img }) => {
+
     return (
         <figure>
-            <img className="img-fluid" src={photo} alt="photos"/>
+    
+            <button type="button" data-toggle="modal" data-target="#exampleModalCenter">
+                    <img className="img-fluid" src={img} alt="photos" />
+            </button>
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <img className="img-fluid" src={img} alt="photos" />
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">cerrar</button>
+                            <button type="button" class="btn btn-primary">Guardar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </figure>
     )
 }
